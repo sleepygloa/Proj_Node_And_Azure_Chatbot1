01e1bfc1dce3c81409e50ed59ca1aca8df53b279
sleepygloa <sleepygloa@gmail.com>
2017-03-26 17:18:13 +0900

    third commit

--- server.js
+++ server.js
@@ -21,13 +21,45 @@ var port = process.env.PORT || 3000;
 var server = http.createServer(app);
 server.listen(port);
 
+//Notificationhub setup
+var azure = require('azure');
+var hubName = 'sleepygloanodewithazurenotify';
+var connectionString = 'Endpoint=sb://sleepygloanodewithazurenotify.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=42i102VfBC+4I9FwsBeGDPfZgrGyRA14YpdBB6CrDEc=';
+var notificationHubService = azure.createNotificationHubService(hubName,connectionString);
+
+
 //socket io 는 클라이언트와 서버가 계속 실시간으로 뭔가를 할수있는 모듈
 var io = require('socket.io').listen(server);
-io.sockets.on('connection',function(socket){ //on 이벤트의 요청을 받을 당시,connection의 경우, function실행
-    socket.emit('toclient',{msg:'Welcome !'});
-    socket.on('fromclient',function(data){
-        socket.broadcast.emit('toclient',data); // 자신을 제외하고 다른 클라이언트에게 보냄
-        socket.emit('toclient',data); // 해당 클라이언트에게만 보냄. 다른 클라이언트에 보낼려면?
-        console.log('Message from client :'+data.msg);
+io.sockets.on('connection',function(socket){
+    socket.emit('toclient',{msg:'Welcome!'});
+    notificationHubService.gcm.send(null, {data:{id:socket.id, message:'Welcome'}},function(error){
+        if(!error){
+            console.log('send');
+        }
     });
+
+    socket.on('fromclient',function(data){
+        socket.broadcast.emit('toclient',data); 
+        socket.emit('toclient',data);
+        console.log('Message from client :'+data.msg);
+       
+        if(!data.msg==""){
+            notificationHubService.gcm.send(null, {data:{id:socket.id, message:data.msg}}, function(error){
+                if(!error){
+                    //notification sent
+                        console.log('send');
+                }
+            });
+        }
+    })
 });
+
+// var io = require('socket.io').listen(server);
+// io.sockets.on('connection',function(socket){ //on 이벤트의 요청을 받을 당시,connection의 경우, function실행
+//     socket.emit('toclient',{msg:'Welcome !'});
+//     socket.on('fromclient',function(data){
+//         socket.broadcast.emit('toclient',data); // 자신을 제외하고 다른 클라이언트에게 보냄
+//         socket.emit('toclient',data); // 해당 클라이언트에게만 보냄. 다른 클라이언트에 보낼려면?
+//         console.log('Message from client :'+data.msg);
+//     });
+// });
